82bdd630b8894f745aaf51d76497a13949472c7d
upwardsw <13517078184@139.com>
2018-10-12 17:11:11 +0800

    test

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-app.secret_key='fsdfshfsdgfr346r93hrfwe8vh3bi4r7834rtn2cir5hv728'
-ALLOWED_EXTENSIONS = ('png', 'jpg', 'jpeg', 'gif', 'docx')
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+app.secret_key = 'fsdfshfsdgfr346r93hrfwe8vh3bi4r7834rtn2cir5hv728'
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'static')
 
 
-@app.route('/',methods=['GET','POST'])
-def hello_world():
-    if request.method=='POST':
-        file=request.files['file']
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
         if file:
-            file.save(os.path.join(os.getcwd(),'static'), file.filename)
+            filename = secure_filename(file.filename)
+            # print(os.path.join(os.getcwd(),'static', file.filename))
+            # file.save(os.path.join(os.getcwd(),'static',filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return 'okk'
     else:
         return render_template('uploadtest.html')
